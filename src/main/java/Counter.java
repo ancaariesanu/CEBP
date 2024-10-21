c49f063ec9fa2900b3c0432a0524165b9c0107e9
fericf <eric.florea@student.upt.ro>
2024-10-21 18:50:49 +0300

    Updated Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -17,11 +17,28 @@ public class Counter {
     {
         Name = name;
         this.documentList = documentList;
-        //here we generate the new counter
+        // here we generate the new counter
     }
 
     public void issueDocument()
     {
-        //here we call the document method to solve the issue
+        // here we call the document method to solve the issue
     }
+
+    public void goOnBreak(int time)
+    {
+        // function to make the counter go on break for a set ammount of time
+    }
+
+    public void resumeWork()
+    {
+        // function to make the counter come back from the break
+    }
+
+    public void postBreakAnnouncement()
+    {
+        // function to announce the server that the counter is closed due to the break.
+    }
+
+
 }
