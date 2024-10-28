625f4f096644a26aa256ac3fd1680e8ba7fc4a17
ancaariesanu <126492545+ancaariesanu@users.noreply.github.com>
2024-10-28 14:01:15 +0200

    Edit the current files, main functionality works

---
+++ src/main/java/Document.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Document {
+    private final String name;
+    private final List<Document> dependencies;
+
+    public Document(String name, List<Document> dependencies) {
+        this.name = name;
+        this.dependencies = dependencies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Document> getDependencies() {
+        return dependencies;
+    }
+
+    public boolean canBeIssued(Customer customer) {
+        for (Document dependency : dependencies) {
+            if (!customer.hasDocument(dependency)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
