613a26fd48640eb201a339316de0512bbfbcfc25
Florea Eric <ericflaviu.florea@gmail.com>
2025-01-09 23:11:22 +0200

    Added Windows Forms Application

---
+++ WpfApp1/WpfApp1/UserControls/AdminPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : UserControl
+    {
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+        private void Sidebar_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Sidebar.Width = 100; 
+            Sidebar.Background= new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cbfb55")); 
+            Dashboard.Visibility = Visibility.Visible; 
+            Settings.Visibility = Visibility.Visible;
+            Profile.Visibility = Visibility.Visible;
+        }
+
+     
+        private void Sidebar_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Sidebar.Width = 50;
+            Sidebar.Background =Brushes.Transparent;
+            Dashboard.Visibility = Visibility.Collapsed; 
+            Settings.Visibility = Visibility.Collapsed;
+            Profile.Visibility = Visibility.Collapsed;
+        }
+
+        private void sidebarMenuButton(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Name == "Dashboard")
+            {
+                MessageBox.Show("pressed");
+            }
+            else if (btn.Name == "Settings")
+            {
+                MessageBox.Show("pressed");
+            }
+            else if (btn.Name == "Profile")
+            {
+                MessageBox.Show("pressed");
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/UserControls/LoginPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : UserControl
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        public void ChangePageFunction(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.ClassName = "User";
+            MainWindow instance = new MainWindow();
+
+        }
+        public void eraseText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Text= string.Empty;
+        }
+        public void getTextBack(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == string.Empty)
+            {
+                if (textBox.Name == "Username")
+                {
+                    textBox.Text = "USERNAME";
+                }
+                else 
+                {
+                    textBox.Text = "PASSWORD";
+                }
+            }
+        }
+        public void enterInPage(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (Username.Text == "admin" && Password.Text == "admin")
+                {
+                    MessageBox.Show("GOOD CRED", "Succes");
+                    var mainwindow = (MainWindow)Application.Current.MainWindow;
+                    mainwindow.FirstContent.Content = new AdminPage();
+                } 
+                else if (Username.Text == "user" && Password.Text == "user")
+                {
+                    MessageBox.Show("GOOD CRED", "Succes");
+                    var mainwindow = (MainWindow)Application.Current.MainWindow;
+                    mainwindow.FirstContent.Content = new UserPage();
+                }
+                else
+                {
+                    Username.Text = string.Empty;
+                    Password.Text = string.Empty;
+                    MessageBox.Show("BAD CRED", "Wrong input");
+                }
+            }
+            
+        }
+
+    }
+}
